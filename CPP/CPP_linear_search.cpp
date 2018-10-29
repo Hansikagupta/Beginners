dcbf307d7c8ab325a76537da41d341d735a5cd38
Hansikagupta <35623890+Hansikagupta@users.noreply.github.com>
2018-10-30 01:14:46 +0530

    Update CPP_linear_search.cpp

--- CPP/CPP_linear_search.cpp
+++ CPP/CPP_linear_search.cpp
@@ -1,34 +1,30 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-int main()
-{
-   
-  int arr[10], i, num, n, c=0, pos;
-  cout<<"Enter the array size : ";
-  cin>>n;
-  cout<<"Enter Array Elements : ";
-  for(i=0; i<n; i++)
-  {
-    cin>>arr[i];
+
+
+void linearSearch(int* arr, int size, int num);
+int main() {
+  int num = 2, size = 5;
+//   cout<<"Enter the array size: ";
+  //cin>>size;
+  int arr[] =  {9, 3, 5, 2, 1 };
+//   cout<<"Enter Array Elements: ";
+//   for(int i = 0; i < size; i++){
+//     cin>>arr[i];
+//   }
+  cout<<"Enter the number to be searched: ";
+//  cin>>num;
+  linearSearch(arr, size, num);
+  return 0;
+}
+
+void linearSearch(int* arr, int size, int num) {
+  for(int i = 0; i < size; i++) {
+      if(arr[i] == num) {
+         cout<<num<<" found at position "<<i+1;
+         return;
+      }
   }
-  cout<<"Enter the number to be search : ";
-  cin>>num;
-  for(i=0; i<n; i++)
-  {
-    if(arr[i]==num)
-    {
-      c=1;
-      pos=i+1;
-      break;
-    }
-  }
-  if(c==0)
-  {
-    cout<<"Number not found..!!";
-  }
-  else
-  {
-    cout<<num<<" found at position "<<pos;
-  }
-    return 0;
+  cout<<"Number not found..!!";
 }
